80a3b499dbd9525ac1121a4080cb91e57a7ad704
moniikagill <37440058+moniikagill@users.noreply.github.com>
2018-08-06 15:41:54 +1200

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,17 @@
+
+public class Card {
+
+	public final CARDTYPE type;	
+	public final String name;
+    public Card(CARDTYPE cardtype, String name) {
+		this.type = cardtype;
+		this.name = name;
+	}
+
+	
+
+	public enum CARDTYPE {
+		CHARACTER, WEAPON, ROOM
+	}
+}
+
